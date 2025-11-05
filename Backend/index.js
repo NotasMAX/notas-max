40e299cab955b79b21e6fe88b3e5a9ad8fe87d64
Evelyn Cassinotte <e.cass03@gmail.com>
2025-11-05 09:57:20 -0300

    feat: set up Express server with CORS and routing

---
+++ Backend/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import routes from "./Routes/routes.js";
+
+const app = new express();
+
+app.use(express.json())
+
+app.use(cors({
+    credentials:true, 
+    origin:"http://localhost:5173"
+}))
+
+app.use("/NotasMax", routes);
+app.listen(5000);
