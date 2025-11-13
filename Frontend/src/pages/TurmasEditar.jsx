5f2fee506b719b0cd842f67e34a490c11459032a
Evelyn Cassinotte <cassinotte@gmail.com>
2025-11-13 19:35:31 -0300

    feat: refactor TurmaAlunoItem and TurmasEditar components; replace styles and integrate TurmaDisciplinaItem component

--- Frontend/src/pages/TurmasEditar.jsx
+++ Frontend/src/pages/TurmasEditar.jsx
@@ -3,15 +3,17 @@ import { useParams } from 'react-router-dom';
 import Style from '../styles/TurmasEditar.module.css';
 // import TurmasForm from '../components/TurmasForm';
 import TurmaAlunoItem from '../components/TurmaAlunoItem';
+import TurmaDisciplinaItem from '../components/TurmaDisciplinaItem';
 import { getTurmaById } from '../api/turmasapi';
 import { useNavigate } from 'react-router-dom';
+import { ConfirmDialog } from 'primereact/confirmdialog';
 
 export default function TurmasEditar() {
     const { id } = useParams();
     const [turma, setTurma] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
-        const navigate = useNavigate();
+    const navigate = useNavigate();
 
     const fetch = async () => {
         try {
@@ -47,6 +49,7 @@ export default function TurmasEditar() {
 
     return (
         <div>
+            <ConfirmDialog />
             <h2 className={Style.TurmasEditarHeader}>
                 Editar Turma - {turma.serie}º EM
             </h2>
@@ -73,14 +76,14 @@ export default function TurmasEditar() {
                 </div>
             </div>
             <div className={Style.TurmaContainerDados}>
-              <TurmaAlunoItem aluno={
-                { nome: "Nome do Aluno", email: "email@exemplo.com" }
-              }></TurmaAlunoItem>
+                <TurmaAlunoItem aluno={
+                    { nome: "Nome do Aluno", email: "email@exemplo.com" }
+                }></TurmaAlunoItem>
             </div>
             <div className={Style.TurmaContainer}>
                 <div className={Style.TurmaContainerHeader}>
                     <h3 className={Style.TurmaContainerTitle}>
-                        Materias
+                        Disciplinas
                     </h3>
                     <button className={Style.TurmaContainerButton}>
                         <svg className={Style.TurmaContainerButtonIcon} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -92,20 +95,9 @@ export default function TurmasEditar() {
                 </div>
             </div>
             <div className={Style.TurmaContainerDados}>
-                <div className={Style.TurmaContainerRow}>
-                    <div className={Style.TurmaContainerCol}>
-                        Nome da Matéria
-                    </div>
-                    <div className={Style.TurmaContainerCol}>
-                        Nome do Professor
-                    </div>
-                    <div className={Style.TurmaContainerColExcluir}>
-                        <svg className={Style.TurmaContainerColIcon} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path fillRule="evenodd" clipRule="evenodd" d="M2 7C2 4.23858 4.23858 2 7 2H17C19.7614 2 22 4.23858 22 7V17C22 19.7614 19.7614 22 17 22H7C4.23858 22 2 19.7614 2 17V7ZM7 4C5.34315 4 4 5.34315 4 7V17C4 18.6569 5.34315 20 7 20H17C18.6569 20 20 18.6569 20 17V7C20 5.34315 18.6569 4 17 4H7Z" />
-                            <path fillRule="evenodd" clipRule="evenodd" d="M8.29289 8.29289C8.68342 7.90237 9.31658 7.90237 9.70711 8.29289L12 10.5858L14.2929 8.29289C14.6834 7.90237 15.3166 7.90237 15.7071 8.29289C16.0976 8.68342 16.0976 9.31658 15.7071 9.70711L13.4142 12L15.7071 14.2929C16.0976 14.6834 16.0976 15.3166 15.7071 15.7071C15.3166 16.0976 14.6834 16.0976 14.2929 15.7071L12 13.4142L9.70711 15.7071C9.31658 16.0976 8.68342 16.0976 8.29289 15.7071C7.90237 15.3166 7.90237 14.6834 8.29289 14.2929L10.5858 12L8.29289 9.70711C7.90237 9.31658 7.90237 8.68342 8.29289 8.29289Z" />
-                        </svg>
-                    </div>
-                </div>
+                <TurmaDisciplinaItem disciplina={
+                    { professor: "Nome do Professor", materia: "Nome da Matéria" }
+                }></TurmaDisciplinaItem>
             </div>
             <button
                 type="button"
