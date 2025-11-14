f5985a269a667f6bddccccd5c7ac86dbfaca208d
unknown <viniciusgimecop@gmail.com>
2025-11-14 15:44:48 -0300

    Cadastro de matérias

--- Backend/Routes/routes.js
+++ Backend/Routes/routes.js
@@ -1,8 +1,6 @@
 import { Router } from "express";
 import TurmasController from "../Controllers/TurmasController.js";
 import materiaController from "../Controllers/materiaController.js";
-import alunoController from "../Controllers/alunoController.js";
-import professorController from "../Controllers/professorController.js";
 
 const routes = Router();
 
@@ -19,17 +17,4 @@ routes.get("/Materias", materiaController.listarMaterias);
 routes.get("/Materias/:id", materiaController.buscarMateriaPorId);
 routes.put("/Materias/Editar/:id", materiaController.editarMateria);
 
-//rotas de Alunos
-routes.post("/Alunos/Cadastrar", alunoController.criarAluno);
-
-routes.get("/Alunos", alunoController.listarAlunos);
-routes.get("/Alunos/:id", alunoController.buscarAlunoPorId);
-routes.put("/Alunos/Editar/:id", alunoController.editarAluno);
-
-//rotas de Professores
-routes.post("/Professores/Cadastrar", professorController.criarProfessor);
-
-routes.get("/Professores", professorController.listarProfessores);
-routes.get("/Professores/:id", professorController.buscarProfessorPorId);
-routes.put("/Professores/Editar/:id", professorController.editarProfessor);
 export default routes;
