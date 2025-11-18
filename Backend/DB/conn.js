8cb4e5bd5e64c06b72d1621c710ecb757aceab6d
Evelyn Cassinotte <e.cass03@gmail.com>
2025-11-18 11:39:42 -0300

    feat: Add Docker support with docker-compose, Dockerfiles for backend and frontend

--- Backend/DB/conn.js
+++ Backend/DB/conn.js
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
 
+const mongoUrl = process.env.MONGODB_URL || "mongodb://127.0.0.1:27017/NotasMax";
+
 async function main(){
-    await mongoose.connect("mongodb://127.0.0.1:27017/NotasMax");
+    await mongoose.connect(mongoUrl);
     console.log("Conectou mongodb");
 }
 main().catch((err)=>console.log(err));
