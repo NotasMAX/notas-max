f3e43b13f12067454a4e06ed8a9d6d44f978dde9
Evelyn Cassinotte <cassinotte@gmail.com>
2025-11-22 11:51:47 -0300

    fix: update API base URL to correct port

--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/NotasMax",
+    baseURL: "http://localhost:30005/NotasMax",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json"
