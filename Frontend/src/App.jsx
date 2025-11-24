38ab9535975dee834bb2e15d6ccd51d9c4893979
Evelyn Cassinotte <cassinotte@gmail.com>
2025-11-24 14:35:57 -0300

    feat: wrap App component with BrowserRouter for routing support

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter ,Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider, useAuth } from './context/AuthContext';
 import Layout from "./components/Layout";
 import Turmas from "./pages/Turmas";
@@ -54,282 +54,278 @@ const GuestRoute = ({ children }) => {
   return children;
 };
 
-function AppRoutes() {
-  return (
-    <Routes>
-
-      <Route path="/reset-senha" element={
-        <GuestRoute>
-          <ResetSenha />
-        </GuestRoute>
-      } />
-
-      <Route path="/Login" element={
-        <GuestRoute>
-          <Login />
-        </GuestRoute>
-      } />
-
-      <Route path="/Home" element={
-        <AdminRoute>
-          <Layout>
-            <Home />
-          </Layout>
-        </AdminRoute>
-      } />
-
-      <Route path="/" element={
-        <AdminRoute>
-          <Layout>
-            <Home />
-          </Layout>
-        </AdminRoute>
-      } />
-
-      <Route path="/recuperar-senha" element={
-        <GuestRoute>
-          <RecuperarSenha />
-        </GuestRoute>
-      } />
-
-      <Route path="Turmas/Cadastrar" element={
-        <AdminRoute>
-          <Layout>
-            <TurmasCadastrar />
-          </Layout>
-        </AdminRoute>
-      } />
-
-      <Route path="Turmas/:ano?" element={
-        <AdminRoute>
-          <Layout>
-            <Turmas />
-          </Layout>
-        </AdminRoute>
-      } />
-
-      <Route path="Turmas/Editar/:id" element={
-        <AdminRoute>
-          <Layout>
-            <TurmasEditar />
-          </Layout>
-        </AdminRoute>
-      } />
-
-      <Route path="Turmas/Info/:id" element={
-        <Layout>
-          <TurmasSimulados />
-        </Layout>
-      } />
-      
-      <Route path="Turma/:id" element={
-        <AdminRoute>
-          <Layout>
-            <TurmasSimulados />
-          </Layout>
-        </AdminRoute>
-      } />
-
-       <Route path="Turmas/:turma/Simulados/:simulado?/Notas/:aluno?" element={
-        <AdminRoute>
-          <Layout>
-            <SimuladosNotas />
-          </Layout>
-        </AdminRoute>
-      } />
-
-
-      <Route path="Turmas/Simulado/Aluno/:id" element={
-        <Layout>
-          <SimuladosAlunosList />
-        </Layout>
-      } />
-        
-      <Route path="/Simulados" element={
-        <AdminRoute>
-          <Layout>
-            <Simulados />
-          </Layout>
-        </AdminRoute>
-      } />
-
-      <Route path="/Simulados/:bimestre/:ano/:serie?" element={
-        <AdminRoute>
-          <Layout>
-            <Simulados />
-          </Layout>
-        </AdminRoute>
-      } />
-
-      <Route path="/Simulados/Notas/:simulado/:aluno" element={
-        <AdminRoute>
-          <Layout>
-            <SimuladosNotas />
-          </Layout>
-        </AdminRoute>
-      } />
-
-
-      <Route path="/Simulados/Cadastrar" element={
-        <AdminRoute>
-          <Layout>
-            <SimuladosCadastrar />
-          </Layout>
-        </AdminRoute>
-      } />
-
-      <Route path="/Simulados/Editar/:id" element={
-        <AdminRoute>
-          <Layout>
-            <SimuladosEditar />
-          </Layout>
-        </AdminRoute>
-      } />
-
-      <Route path="/Simulados/Notas/:simuladoIdParams/:bimestre/:alunoId" element={
-        <Layout>
-          <SimuladoNotasPorSimulado />
-        </Layout>
-      } />
-
-      <Route path="/Simulados/Notas/Materia/:simuladoIdParams/:bimestre/:alunoId" element={
-        <Layout>
-          <SimuladoNotasPorMateria />
-        </Layout>
-      } />
-
-      <Route path="/Materias/Cadastrar" element={
-        <AdminRoute>
-          <Layout>
-            <MateriaCadastrar />
-          </Layout>
-        </AdminRoute>
-      } />
-
-      <Route path="/Materias" element={
-        <AdminRoute>
-          <Layout>
-            <MateriaListar />
-          </Layout>
-        </AdminRoute>
-      } />
-
-      <Route path="/Materias/Editar/:id" element={
-        <AdminRoute>
-          <Layout>
-            <MateriaEditar />
-          </Layout>
-        </AdminRoute>
-      } />
-
-      {/* Rotas de Alunos */}
-      <Route path="/Alunos/Cadastrar" element={
-        <AdminRoute>
-          <Layout>
-            <AlunoCadastrar />
-          </Layout>
-        </AdminRoute>
-      } />
-
-      <Route path="/Alunos" element={
-        <AdminRoute>
-          <Layout>
-            <AlunoListar />
-          </Layout>
-        </AdminRoute>
-      } />
-
-      <Route path="/Alunos/Visualizar/:id" element={
-        <AdminRoute>
-          <Layout>
-            <AlunoVisualizar />
-          </Layout>
-        </AdminRoute>
-      } />
-
-      <Route path="/Alunos/Editar/:id" element={
-        <AdminRoute>
-          <Layout>
-            <AlunoEditar />
-          </Layout>
-        </AdminRoute>
-      } />
-
-      {/* Rotas de Professores */}
-      <Route path="/Professores/Cadastrar" element={
-        <AdminRoute>
-          <Layout>
-            <ProfessorCadastrar />
-          </Layout>
-        </AdminRoute>
-      } />
-
-      <Route path="/Professores" element={
-        <AdminRoute>
-          <Layout>
-            <ProfessorListar />
-          </Layout>
-        </AdminRoute>
-      } />
-
-      <Route path="/Professores/Visualizar/:id" element={
-        <AdminRoute>
-          <Layout>
-            <ProfessorVisualizar />
-          </Layout>
-        </AdminRoute>
-      } />
-
-      <Route path="/Professores/Editar/:id" element={
-        <AdminRoute>
-          <Layout>
-            <ProfessorEditar />
-          </Layout>
-        </AdminRoute>
-      } />
-
-      <Route path="/Usuarios/Aluno/:id/desempenho" element={
-        <AdminRoute>
-          <Layout>
-            <AlunoDesempenho />
-          </Layout>
-        </AdminRoute>
-
-      } />
-
-      <Route path="/Turma/:id/desempenho" element={
-        <AdminRoute>
-          <Layout>
-            <TurmaDesempenho />
-          </Layout>
-        </AdminRoute>
-      } /> 
-
-      <Route path="/Turma/:id/desempenho-materias" element={
-        <AdminRoute>
-          <Layout>
-            <TurmasAcertosPorMateria />
-          </Layout>
-        </AdminRoute>
-      } />
-
-      <Route path="/404" element={
-        <NotFound />
-      } />
-
-      <Route path="*" element={
-        <Navigate to="/404" replace />
-      } />
-
-    </Routes>
-  );
-}
-
 function App() {
   return (
     <AuthProvider>
-      <AppRoutes />
+      <BrowserRouter>
+        <Routes>
+
+          <Route path="/reset-senha" element={
+            <GuestRoute>
+              <ResetSenha />
+            </GuestRoute>
+          } />
+
+          <Route path="/Login" element={
+            <GuestRoute>
+              <Login />
+            </GuestRoute>
+          } />
+
+          <Route path="/Home" element={
+            <AdminRoute>
+              <Layout>
+                <Home />
+              </Layout>
+            </AdminRoute>
+          } />
+
+          <Route path="/" element={
+            <AdminRoute>
+              <Layout>
+                <Home />
+              </Layout>
+            </AdminRoute>
+          } />
+
+          <Route path="/recuperar-senha" element={
+            <GuestRoute>
+              <RecuperarSenha />
+            </GuestRoute>
+          } />
+
+          <Route path="Turmas/Cadastrar" element={
+            <AdminRoute>
+              <Layout>
+                <TurmasCadastrar />
+              </Layout>
+            </AdminRoute>
+          } />
+
+          <Route path="Turmas/:ano?" element={
+            <AdminRoute>
+              <Layout>
+                <Turmas />
+              </Layout>
+            </AdminRoute>
+          } />
+
+          <Route path="Turmas/Editar/:id" element={
+            <AdminRoute>
+              <Layout>
+                <TurmasEditar />
+              </Layout>
+            </AdminRoute>
+          } />
+
+          <Route path="Turmas/Info/:id" element={
+            <Layout>
+              <TurmasSimulados />
+            </Layout>
+          } />
+
+          <Route path="Turma/:id" element={
+            <AdminRoute>
+              <Layout>
+                <TurmasSimulados />
+              </Layout>
+            </AdminRoute>
+          } />
+
+          <Route path="Turmas/:turma/Simulados/:simulado?/Notas/:aluno?" element={
+            <AdminRoute>
+              <Layout>
+                <SimuladosNotas />
+              </Layout>
+            </AdminRoute>
+          } />
+
+
+          <Route path="Turmas/Simulado/Aluno/:id" element={
+            <Layout>
+              <SimuladosAlunosList />
+            </Layout>
+          } />
+
+          <Route path="/Simulados" element={
+            <AdminRoute>
+              <Layout>
+                <Simulados />
+              </Layout>
+            </AdminRoute>
+          } />
+
+          <Route path="/Simulados/:bimestre/:ano/:serie?" element={
+            <AdminRoute>
+              <Layout>
+                <Simulados />
+              </Layout>
+            </AdminRoute>
+          } />
+
+          <Route path="/Simulados/Notas/:simulado/:aluno" element={
+            <AdminRoute>
+              <Layout>
+                <SimuladosNotas />
+              </Layout>
+            </AdminRoute>
+          } />
+
+
+          <Route path="/Simulados/Cadastrar" element={
+            <AdminRoute>
+              <Layout>
+                <SimuladosCadastrar />
+              </Layout>
+            </AdminRoute>
+          } />
+
+          <Route path="/Simulados/Editar/:id" element={
+            <AdminRoute>
+              <Layout>
+                <SimuladosEditar />
+              </Layout>
+            </AdminRoute>
+          } />
+
+          <Route path="/Simulados/Notas/:simuladoIdParams/:bimestre/:alunoId" element={
+            <Layout>
+              <SimuladoNotasPorSimulado />
+            </Layout>
+          } />
+
+          <Route path="/Simulados/Notas/Materia/:simuladoIdParams/:bimestre/:alunoId" element={
+            <Layout>
+              <SimuladoNotasPorMateria />
+            </Layout>
+          } />
+
+          <Route path="/Materias/Cadastrar" element={
+            <AdminRoute>
+              <Layout>
+                <MateriaCadastrar />
+              </Layout>
+            </AdminRoute>
+          } />
+
+          <Route path="/Materias" element={
+            <AdminRoute>
+              <Layout>
+                <MateriaListar />
+              </Layout>
+            </AdminRoute>
+          } />
+
+          <Route path="/Materias/Editar/:id" element={
+            <AdminRoute>
+              <Layout>
+                <MateriaEditar />
+              </Layout>
+            </AdminRoute>
+          } />
+
+          {/* Rotas de Alunos */}
+          <Route path="/Alunos/Cadastrar" element={
+            <AdminRoute>
+              <Layout>
+                <AlunoCadastrar />
+              </Layout>
+            </AdminRoute>
+          } />
+
+          <Route path="/Alunos" element={
+            <AdminRoute>
+              <Layout>
+                <AlunoListar />
+              </Layout>
+            </AdminRoute>
+          } />
+
+          <Route path="/Alunos/Visualizar/:id" element={
+            <AdminRoute>
+              <Layout>
+                <AlunoVisualizar />
+              </Layout>
+            </AdminRoute>
+          } />
+
+          <Route path="/Alunos/Editar/:id" element={
+            <AdminRoute>
+              <Layout>
+                <AlunoEditar />
+              </Layout>
+            </AdminRoute>
+          } />
+
+          {/* Rotas de Professores */}
+          <Route path="/Professores/Cadastrar" element={
+            <AdminRoute>
+              <Layout>
+                <ProfessorCadastrar />
+              </Layout>
+            </AdminRoute>
+          } />
+
+          <Route path="/Professores" element={
+            <AdminRoute>
+              <Layout>
+                <ProfessorListar />
+              </Layout>
+            </AdminRoute>
+          } />
+
+          <Route path="/Professores/Visualizar/:id" element={
+            <AdminRoute>
+              <Layout>
+                <ProfessorVisualizar />
+              </Layout>
+            </AdminRoute>
+          } />
+
+          <Route path="/Professores/Editar/:id" element={
+            <AdminRoute>
+              <Layout>
+                <ProfessorEditar />
+              </Layout>
+            </AdminRoute>
+          } />
+
+          <Route path="/Usuarios/Aluno/:id/desempenho" element={
+            <AdminRoute>
+              <Layout>
+                <AlunoDesempenho />
+              </Layout>
+            </AdminRoute>
+
+          } />
+
+          <Route path="/Turma/:id/desempenho" element={
+            <AdminRoute>
+              <Layout>
+                <TurmaDesempenho />
+              </Layout>
+            </AdminRoute>
+          } />
+
+          <Route path="/Turma/:id/desempenho-materias" element={
+            <AdminRoute>
+              <Layout>
+                <TurmasAcertosPorMateria />
+              </Layout>
+            </AdminRoute>
+          } />
+
+          <Route path="/404" element={
+            <NotFound />
+          } />
+
+          <Route path="*" element={
+            <Navigate to="/404" replace />
+          } />
+        </Routes>
+      </BrowserRouter>
     </AuthProvider>
+
   );
 }
 
