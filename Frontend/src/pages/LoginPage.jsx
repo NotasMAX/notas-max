5f1a808d47ccafb2c8c02432259b1e8e29f9edd9
Evelyn Cassinotte <cassinotte@gmail.com>
2025-11-25 11:54:43 -0300

    feat: implement password recovery form and integrate toast notifications for user feedback

--- Frontend/src/pages/LoginPage.jsx
+++ Frontend/src/pages/LoginPage.jsx
@@ -1,6 +1,29 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import LoginForm from '../components/LoginForm';
+import { Toast } from 'primereact/toast';
+import { useLocation } from 'react-router-dom';
 
 export default function LoginPage() {
-  return <LoginForm />;
+  const toast = useRef(null);
+  const location = useLocation();
+  const toastShown = useRef(false);
+
+  useEffect(() => {
+    if (location.state && !toastShown.current) {
+      const { message, type } = location.state;
+      if (message && type) {
+        if (toast && toast.current) {
+          toast.current.show({ severity: type, summary: 'Informação', detail: message, life: 8000 });
+          toastShown.current = true;
+        }
+      }
+      window.history.replaceState({}, '')
+    }
+  }, [location.state]);
+
+  return (
+    <div>
+      <Toast ref={toast} />
+      <LoginForm />;
+    </div>)
 }
--- Frontend/src/pages/RecuperarSenha.jsx
+++ Frontend/src/pages/RecuperarSenha.jsx
@@ -1,44 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import styles from "../styles/Login.module.css";
 import logo from "../imgs/logo.svg";
-import { Link } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
+import RecuperarSenhaForm from "../components/RecuperarSenhaForm";
+import { Toast } from 'primereact/toast';
 
 
 export default function RecuperarSenha() {
-  const [email, setEmail] = useState("");
-  const [mensagem, setMensagem] = useState("");
+  const toast = useRef(null);
+  const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSubmit = (dados) => {
 
-    if (!email.trim()) {
-      setMensagem("Por favor, insira seu e-mail institucional.");
-      return;
-    }
+    //lógica para enviar o e-mail de recuperação de senha
+
+    navigate('/login', { state: { message: 'Se o e-mail informado for válido, um token de recuperação de senha será enviado para sua caixa de entrada.', type: 'info' } });
   };
 
   return (
     <div className={styles.container}>
+      <Toast ref={toast} />
       <div className={styles.card}>
         <img src={logo} alt="Logo" className={styles.logo} />
         <h2 className={styles.title}>Recuperação de Senha</h2>
-
-        {mensagem && <p className={styles.error}>{mensagem}</p>}
-
-        <form onSubmit={handleSubmit} className={styles.form}>
-          <label className={styles.label}>Informe o E-mail institucional</label>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className={styles.input}
-            placeholder="exemplo@email.com"
-            required
-          />
-<button type="submit" className={styles.button}>
-  <Link to="/reset-senha">Enviar Link</Link> </button>
-        </form>
-
+        <RecuperarSenhaForm onSubmit={handleSubmit} />
         <div className={styles.recover}>
           <a href="/login">Voltar</a>
         </div>
